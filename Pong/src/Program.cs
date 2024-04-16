b59280ce9ed924692cf8a521021ecb0119b4560a
4-en <ben.schumacher0@gmail.com>
2024-04-16 18:04:14 +0200

    setup

---
+++ Pong/src/Program.cs
@@ -0,0 +1,15 @@
+﻿using SDL2Engine;
+
+namespace Pong
+{
+    internal class Pong
+    {
+        static void Main(string[] args)
+        {
+            var scene = SDL2Engine.Program.CreateScene();
+
+            var engine = new Engine(scene);
+            engine.Run();
+        }
+    }
+}
