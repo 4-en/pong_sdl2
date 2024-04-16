9e698a26de97df52e844788412d56e9d39d397de
4-en <ben.schumacher0@gmail.com>
2024-04-16 18:18:44 +0200

    add comment for example

--- Pong/src/Program.cs
+++ Pong/src/Program.cs
@@ -6,9 +6,15 @@ namespace Pong
     {
         static void Main(string[] args)
         {
+            // creates a simple scene with a rotating rectangle that follows the mouse
+            // left click to spawn a new rectangle at the mouse position
             var scene = SDL2Engine.EngineTest.CreateScene();
 
+            // use engine class to run a scene
+            // contains the game loop and window creation
             var engine = new Engine(scene);
+
+            // create the window and run the game loop until window is closed
             engine.Run();
         }
     }
